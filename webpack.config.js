54c4710323635a331814fc35048c399bf8cc8325
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-12-26 19:24:47 -0600

    added example configs to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: {
     carousel: './src/components/Carousel.js',
+    example: './example/index.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+    title: 'Carousel Example',
+    chunks: ['example'] }),
+    ],
   module: {
     rules: [
       {
