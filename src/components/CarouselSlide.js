195b5dd216ddbe9a3a079ba5771c659875282678
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2021-12-26 20:26:33 -0600

    addes styled component pkg

--- src/components/CarouselSlide.js
+++ src/components/CarouselSlide.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const CarouselSlide = ({ imgUrl, attribution, description, ...rest }) => (
+const Img = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: ${(props) =>
+    typeof props.imgHeight === 'number'
+      ? props.imgHeight + 'px'
+      : props.imgHeight};
+`;
+
+const CarouselSlide = ({ imgUrl, imgHeight, attribution, description, ...rest }) => (
   <figure {...rest}>
-    <img src={imgUrl} />
+    <Img src={imgUrl} imgHeight={imgHeight}/>
     <figcaption>
       <strong>{description} </strong>
       {attribution}
@@ -13,8 +23,13 @@ const CarouselSlide = ({ imgUrl, attribution, description, ...rest }) => (
 
 CarouselSlide.propTypes = {
   imgUrl: PropTypes.string.isRequired,
+  imgHeight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   attribution: PropTypes.string,
   description: PropTypes.string,
+};
+
+CarouselSlide.defaultProps = {
+  imgHeight: 500,
 };
 
 export default CarouselSlide;
